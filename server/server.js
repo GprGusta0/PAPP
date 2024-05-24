d3bbd30e743bcf7ea32d38fe929102de2cf916a7
GprGusta0 <100092545+GprGusta0@users.noreply.github.com>
2024-05-24 18:01:24 +0100

    sdadsadassa

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ app.use("/api/upload", Uploadrouter);
 // error handling middleware
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running in  http://localhost/${PORT}`);
